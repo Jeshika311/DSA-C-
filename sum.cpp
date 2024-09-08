982653a43da8535c93f5b0bd2dd32b7cc5dec281
Jeshika311 <jeshikatha@gmail.com>
2024-09-08 13:56:08 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter value of a:";
+    cin>>a;
+    cout<<"enter value of b:";
+    cin>>b;
+
+    int sum = a+b;
+    cout<<"the sum of a and b is:"<<sum;
+    
+}
