cee940f6d5d4cb3e28c18cad086a68d98c2270e2
Jeshika311 <jeshikatha@gmail.com>
2024-09-27 12:07:21 +0530

    Add files via upload

---
+++ PredictNo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+int x;
+x = 765;
+
+if(x>99 && x<1000){
+    cout<<"It is a three digit number"<<endl;
+}
+else{
+    cout<<"It is not a three digit number"<<endl;
+}
+}
